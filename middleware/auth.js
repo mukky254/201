682ab9461ffd8acd9bbee889bd759931b150fd7a
mukky254 <muhidinyusuf06@gmail.com>
2026-01-01 17:31:00 +0300

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    
+    if (!token) {
+      throw new Error();
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Please authenticate' });
+  }
+};
+
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ 
+        error: `Role ${req.user.role} is not authorized to access this resource` 
+      });
+    }
+    next();
+  };
+};
+
+module.exports = { auth, authorize };
